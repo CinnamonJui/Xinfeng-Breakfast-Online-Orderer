30c608b18538ce9ff35e83870c659fef20221225
chette <0chette0@gmail.com>
2019-12-03 16:38:13 +0800

    Update trigger_test.php

--- trigger_test.php
+++ trigger_test.php
@@ -1,16 +1,18 @@
 <?php
 
-    $update_trigget = "create trigger credits_earned after update of takes on (grade)
-    referenceing new row as nrow
-    referencing old row as orow
-    for each row
-    when nrow.grade <> 'F' and nrow.grade is not numfmt_get_locale 
-        and (orow.grade = 'F' or orow.grade is null)
-    begin atomic
-        update student
-        set tot_cred= tot_cred +
-            (select credits
-            from course
-            where course.course_id = nrow.course_id)
-        where student.id=nrow.id;
-    end;";
+    $update_trigger = "
+        create trigger credits_earned after update of takes on (grade)
+            referenceing new row as nrow
+            referencing old row as orow
+            for each row
+            when nrow.grade <> 'F' and nrow.grade is not numfmt_get_locale 
+            and (orow.grade = 'F' or orow.grade is null)
+        
+        begin atomic
+            update student
+            set tot_cred= tot_cred +
+                (select credits
+                from course
+                where course.course_id = nrow.course_id)
+                where student.id=nrow.id;
+        end;";
