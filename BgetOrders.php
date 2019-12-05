f425ca8cfc43ae9174d3b901a7f1f1d193e478b8
chette <0chette0@gmail.com>
2019-12-06 01:50:58 +0800

    Update BgetOrders.php

--- BgetOrders.php
+++ BgetOrders.php
@@ -1,16 +1,18 @@
 <?php
 
-    require_once 'login.php';
     include_once 'Bacon.php';
-    
+    //連接database
     $conn = new Bacon();
     $result;
+    //決定要取歷史訂單還是現時訂單 然後直接取出來
     if(isset($_POST['order_history'])&&$_POST['order_history']=="歷史訂單"){
-        $result = $conn->getFinishOrder();
+        $result = $conn->getFinishOrder(); 
     }
     else if(isset($_POST['order_history'])&&$_POST['order_history']=="現時訂單"){
         $result = $conn->getUnFinishOrder();
     }
+
+    //更新訂單狀態
     if(isset($_POST['order_status'])&&isset($_POST['order_ID'])){
         $order_status=$_POST['order_status'];
         $order_ID=$_POST['order_ID'];
@@ -18,5 +20,6 @@
         $conn->changeOrderStatus($order_ID,$order_status);
     }
 
-    //if($result!="{}") 
-    echo ($result);
+    //回傳資料
+    if($result!="[]") 
+        echo ($result);
