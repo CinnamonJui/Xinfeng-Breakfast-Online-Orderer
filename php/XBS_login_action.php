4f5307d9b1fe057a4608cc7613e268975b4c8485
Chun Wei <dsjfbnkjsbdfkjs@gmail.com>
2020-01-07 18:55:51 +0800

    login_update

--- php/XBS_login_action.php
+++ php/XBS_login_action.php
@@ -1,9 +1,26 @@
 <?php
 
-include_once 'Bacon.php';
+require_once '../core/init.php'; //this code works on register
+
+if (Input::exists()) {
+    $user = new User();
+    $login = $user->login(Input::get('tel'), Input::get('password'));
+    if ($login) {
+        if(Input::get('tel')=='0000000000'){
+            Redirect::to('./../register.html');//to boss
+        }
+        echo true;
+    } else {
+        echo false;
+    }
+}
+
+
+/*include_once 'Bacon.php'; //previous code
 $bacon = new Bacon();
 
 if ($bacon->login($_POST['tel'], $_POST['password']))
     echo true;
 else
-    echo false;
+    echo false;*/
+    
