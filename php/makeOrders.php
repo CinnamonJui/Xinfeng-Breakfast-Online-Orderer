4a9b2cdd1fd9af63995af1a2005379d58bfb7c11
asd8838 <55752475+asd8838@users.noreply.github.com>
2020-01-07 16:34:09 +0800

    1/7
    確認預定時間是否為營業時間

--- php/makeOrders.php
+++ php/makeOrders.php
@@ -7,21 +7,32 @@
         $items = $_POST['item_list'];
         $finishTime = $_POST['finishTime'];
     }
+    $limitTime1 = date("Y-m-d")." "."05:00";
+    $limitTime2 = date("Y-m-d")." "."13:00";
+    $ordertime = date("Y-m-d")." ".$finishTime;
     //echo $items;
     //echo $finishTime;
     //print_r($items);
-    $jsonitems  = json_decode($items,true);
-    //echo $items;
-    $total_price = 0;
-    foreach($jsonitems as $k=> $v){
-      $temp = explode(",",$v); 
-      $total_price  += $temp[1];
-}
-    $getTime = (string)(date("H:i",time()+7*3600));
-    session_start();
-    $userID = $_SESSION['ID'];
-    $orderID = date("Y-m-d")."-".date("H-i",time()+7*3600)."-".$userID;
+
+    if(strtotime($limitTime1)<strtotime($ordertime) && strtotime($limitTime1)<strtotime($limitTime2)){
+        $jsonitems  = json_decode($items,true);
+        //echo $items;
+        $total_price = 0;
+        foreach($jsonitems as $k=> $v){
+        $temp = explode(",",$v); 
+        $total_price  += $temp[1];
+        }
+        $getTime = (string)(date("H:i",time()+7*3600));
+        session_start();
+        $userID = $_SESSION['ID'];
     
-    $conn->addOrder("未確認",$orderID,$total_price,$getTime,$finishTime,$userID,$items);
-    echo ($orderID);
+        $orderID = date("Y-m-d")."-".date("H-i",time()+7*3600)."-".$userID;
+    
+        $conn->addOrder("未確認",$orderID,$total_price,$getTime,$finishTime,$userID,$items);
+        echo ($orderID);
+    }
+    else{
+        echo 0;
+    }
+    
 ?>
